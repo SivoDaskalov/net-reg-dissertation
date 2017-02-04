a5248da20eae320065e741707aa8171cbc4c412a
SivoDaskalov <sivodaskalov@gmail.com>
2017-02-04 19:24:53 +0000

    Grace optimized, betas not sparse - investigate!

--- grace.R
+++ grace.R
@@ -2,49 +2,47 @@
 library(glmnet)
 
 cvGrace <- function(X, Y, L, lambda.L, lambda.1, lambda.2, K = 10){
-    lambda.1 <- unique(sort(lambda.1, decreasing = TRUE))
-    lambda.L <- unique(sort(lambda.L, decreasing = TRUE))
-    lambda.2 <- unique(sort(lambda.2, decreasing = TRUE))
-
-    p <- ncol(X)
-    n <- nrow(X)
-    ERRlist = c()
-    for(iL in 1:length(lambda.L)){
-        lL <- lambda.L[iL]
-        for(i2 in 1:length(lambda.2)){
-            l2 <- lambda.2[i2]
-            Lnew <- lL * L + l2 * diag(p)
-            eL <- eigen(Lnew)
-            
-            if( sum(eL$values<=0)>0 ){next}
-            
-            S <- eL$vectors %*% sqrt(diag(eL$values))
-            l2star <- 1
-            l1star <- lambda.1
-            Xstar <- rbind(X, sqrt(l2star) * t(S)) / sqrt(1 + l2star)
-            Ystar <- c(Y, rep(0, p))
-            gammastar <- l1star / sqrt(1 + l2star) / 2 / (n + p)
-            for( i1 in 1:length(gammastar)    ){
-                temp_gammastar =  c(gammastar[i1] + 0.01, gammastar[i1])
-                cvres <- cv.glmnet(Xstar, Ystar, lambda = temp_gammastar, intercept = FALSE, standardize = FALSE, nfolds = K)
-                if(!is.na(cvres$cvm[2])){
-                    ERRlist = rbind( ERRlist, c( lambda.1[i1] , lL, l2, cvres$cvm[2] )  )
-                }
-            }
-        }
-    }
-    
-    colnames(ERRlist)=c("lambda.1","lambda.L","lambda.2","cvm")
-    return(ERRlist)
-}
-
-grace <- function(Y, X, Ytu, Xtu, L, lambda.L, lambda.1 = 0, lambda.2 = 0, normalize.L = FALSE, K = 10){
+  X <- scale(X)
+  Y <- Y - mean(Y)
+  
   lambda.L <- unique(sort(lambda.L, decreasing = TRUE))
   lambda.1 <- unique(sort(lambda.1, decreasing = TRUE))
   lambda.2 <- unique(sort(lambda.2, decreasing = TRUE))
   
-  ori.Y <- Y
-  ori.X <- X
+  p <- ncol(X)
+  n <- nrow(X)
+  errors = c()
+  
+  for(iL in 1:length(lambda.L)){
+    lL <- lambda.L[iL]
+    for(i2 in 1:length(lambda.2)){
+      l2 <- lambda.2[i2]
+      Lnew <- lL * L + l2 * diag(p)
+      eL <- eigen(Lnew)
+      if( sum(eL$values<=0)>0 ){next}
+      S <- eL$vectors %*% sqrt(diag(eL$values))
+      
+      l2star <- 1
+      l1star <- lambda.1
+      Xstar <- rbind(X, sqrt(l2star) * t(S)) / sqrt(1 + l2star)
+      Ystar <- c(Y, rep(0, p))
+      gammastar <- l1star / sqrt(1 + l2star) / 2 / (n + p)
+      
+      cvres <- cv.glmnet(Xstar, Ystar, lambda = gammastar, intercept = FALSE, standardize = FALSE, nfolds = K)
+      for(i1 in 1:length(gammastar)){
+        if(!is.na(cvres$cvm[i1])){
+          errors <- rbind( errors, c( lambda.1[i1] , lL, l2, cvres$cvm[i1] ) )
+        }
+      }
+    }
+  }
+  
+  colnames(errors)=c("lambda.1","lambda.L","lambda.2","cvm")
+  idxRes = which.min(errors[,4])
+  return(list(errors = errors, lambda.min = errors[idxRes,]))
+}
+
+grace <- function(X, Y, Xtu, Ytu, L, lambda.L, lambda.1 = 0, lambda.2 = 0, normalize.L = FALSE, K = 10){
   if(!is.null(ncol(Y))){
     stop("Error: Y is not a vector.")
   }
@@ -64,11 +62,13 @@ grace <- function(Y, X, Ytu, Xtu, L, lambda.L, lambda.1 = 0, lambda.2 = 0, norma
     stop("Error: At least one of the grace tuning parameters must be positive.")
   }
   
+  ori.Y <- Y
+  ori.X <- X
+  scale.fac <- attr(scale(X), "scaled:scale")
+  X <- scale(X)     # Standardize X
   Y <- Y - mean(Y)  # Center Y
   n <- nrow(X)
   p <- ncol(X)
-  scale.fac <- attr(scale(X), "scaled:scale")
-  X <- scale(X)     # Standardize X
   
   if(normalize.L){
     diag(L)[diag(L) == 0] <- 1
@@ -77,21 +77,11 @@ grace <- function(Y, X, Ytu, Xtu, L, lambda.L, lambda.1 = 0, lambda.2 = 0, norma
   
   # If more than one tuning parameter is provided, perform K-fold cross-validation  
   if((length(lambda.L) > 1) | (length(lambda.1) > 1) | (length(lambda.2) > 1)){
-    Xtu <- scale(Xtu)
-    Ytu <- Y - mean(Y)
-    
     parameters <- cvGrace(Xtu, Ytu, L, lambda.L, lambda.1, lambda.2, K)
-    tun = parameters[parameters[,4]==min(parameters[,4]), ]
-    
-    if(length(dim(tun))==2 ){
-      lambda.1 <- tun[1,1]
-      lambda.L <- tun[1,2]
-      lambda.2 <- tun[1,3]  
-    }else{
-      lambda.1 <- tun[1]
-      lambda.L <- tun[2]
-      lambda.2 <- tun[3] 
-    }
+    tun = parameters$lambda.min
+    lambda.1 <- tun[1]
+    lambda.L <- tun[2]
+    lambda.2 <- tun[3] 
   }
   
   # See Li & Li (2008) for reference
@@ -109,7 +99,7 @@ grace <- function(Y, X, Ytu, Xtu, L, lambda.L, lambda.1 = 0, lambda.2 = 0, norma
 
   truebetahat <- betahat / scale.fac  # Scale back coefficient estimate
   truealphahat <- mean(ori.Y - as.matrix(ori.X) %*% truebetahat)
-  return(list( parameters = list(parameterCV=parameters, parameterMin=tun),
+  return(list( parameters = parameters,
                fit = graceFit,
                coefficients=list(intercept = truealphahat, beta = truebetahat)))
 }
--- main.R
+++ main.R
@@ -3,8 +3,9 @@ source("dataset_tools.R")
 source("evaluation_tools.R")
 source("grace.R")
 
+originalResults = list(sens = 0.75, spec = 0.983, prec = 0.917, cor = 0.838, err = 485.283)
 n = 100
-transFactorsCount = 20
+transFactorsCount = 200
 regulatedGenesPerTF = 10
 
 set.seed(0)
@@ -21,11 +22,13 @@ Xts = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
 Yts = simulateResponse(Xts, Betas)
 
 lambdaGrid = 10 ^ seq(from = -2, by = 1, length = 6)
-grace = grace(Ytr[,1], Xtr, Ytu[,1], Xtu, L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10)
+graceRes = grace(Xtr, Ytr[,1], Xtu, Ytu[,1], L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10)
 
-prediction <- predict(object=grace$fit, as.matrix(Xts), type="response")
-mse = mean((prediction - Yts[,1])^2)
+prediction <- predict(object=graceRes$fit, as.matrix(Xts), type="response")
+error = mean((prediction - Yts[,1])^2)
 
-betaHat = as.numeric(unlist(grace$coefficients$beta))
+betaHat = as.numeric(unlist(graceRes$coefficients$beta))
 trueBeta = as.numeric(Betas[1,])
-evalStatistics(trueBeta = as.numeric(Betas[1,]), betaHat = as.numeric(unlist(grace$coefficients$beta)))
+stat = evalStatistics(trueBeta = as.numeric(Betas[1,]), betaHat = as.numeric(unlist(graceRes$coefficients$beta)))
+tuning = graceRes$parameters$errors
+which.min(tuning[,4])
