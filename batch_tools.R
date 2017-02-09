2415b84253066c2343b9e7996fcc828564341927
SivoDaskalov <sivodaskalov@gmail.com>
2017-02-09 11:49:23 +0000

    Batch infrastructure functioning

--- batch_tools.R
+++ batch_tools.R
@@ -4,25 +4,34 @@ source("evaluation_tools.R")
 source("lasso.R")
 
 batchLasso = function(Ytu, Xtu, Ytr, Xtr, Yts, Xts, lambdas, Betas){
-  
+  cases = list()
   for(i in 1:nrow(Betas)){
-    Ytu[,i]
-    Betas[i,]
     lassoRes = lasso(Ytr[,i], Xtr, Ytu[,i], Xtu, lambda = lambdas, K = 10)
     prediction <- predict(object=lassoRes$fit, as.matrix(Xts), type="response")
-    
+    errors = evalErrors(Yts[,i],prediction)
+    stat = evalBetaStatistics(trueBeta = as.numeric(Betas[i,]), betaHat = as.numeric(unlist(lassoRes$coefficients)))
+    cases[[length(cases)+1]] = list(model = lassoRes, prediction = prediction, errors = errors$errors, mse = errors$mse, stderr = errors$stderr,
+                                    cor = stat$cor, prec = stat$prec, spec = stat$spec, sens = stat$sens)
   }
-  return(list())
+  
+  summary = c()
+  for(i in 1:length(cases)){
+    tmp = c(case = i, sens = cases[[i]]$sens, spec = cases[[i]]$spec, prec = cases[[i]]$prec,  
+            cor = cases[[i]]$cor, mse = cases[[i]]$mse, stderr = cases[[i]]$stderr)
+    summary <- rbind(summary, tmp)
+  }
+  rownames(summary) <- NULL
+  
+  return(list(cases = cases, summary = summary))
 }
 
 runBatch = function(n, factors, genesPerFactor){
   ds = generateDatasets(n, factors, genesPerFactor)
   
-  lassoLambdaGrid = 10 ^ seq(from = -2, by = 1, length = 6)
-  lassoRes = batchLasso(ds$Ytu, ds$Xtu, ds$Ytr, ds$Xtr, ds$Yts, ds$Xts, lambdas = lassoLambdaGrid, ds$Betas)
-  
   models = list()
-  models$lasso = lassoRes
+  
+  lassoLambdaGrid = 10 ^ seq(from = -2, by = 1, length = 6)
+  models$lasso = batchLasso(ds$Ytu, ds$Xtu, ds$Ytr, ds$Xtr, ds$Yts, ds$Xts, lambdas = lassoLambdaGrid, ds$Betas)
   
   result = list()
   result$L = ds$L
--- main.R
+++ main.R
@@ -1,56 +1,63 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("dataset_tools.R")
-source("evaluation_tools.R")
 source("grace.R")
 source("batch_tools.R")
-
-# originalResultsTF20 = list(sens = 0.75, spec = 0.983, prec = 0.917, cor = 0.838, err = 485.283)
-# optimizedResultsTF200 = list(sens = 1, spec = 0.231, prec = 0.025, cor = 0.623, err = 338.914)
 
 n = 100
 transFactorsCount = 200
 regulatedGenesPerTF = 10
 
-set.seed(0)
-L = generateNetwork(transFactorsCount, regulatedGenesPerTF)
-Betas = generateBetas(transFactorsCount, regulatedGenesPerTF)
+batch = runBatch(n, transFactorsCount, regulatedGenesPerTF)
 
-Xtu = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
-Ytu = simulateResponse(Xtu, Betas)
+L = batch$L
+betas = batch$betas
+Xtu = batch$datasets$Xtu
+Ytu = batch$datasets$Ytu
+Xtr = batch$datasets$Xtr
+Ytr = batch$datasets$Ytr
+Xts = batch$datasets$Xts
+Yts = batch$datasets$Yts
 
-Xtr = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
-Ytr = simulateResponse(Xtr, Betas)
+models = batch$models
 
-Xts = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
-Yts = simulateResponse(Xts, Betas)
+lassoModels = models$lasso$cases
+lassoSummary = models$lasso$summary
 
-lambdaGrid = 10 ^ seq(from = -2, by = 1, length = 6)
+# set.seed(0)
+# L = generateNetwork(transFactorsCount, regulatedGenesPerTF)
+# Betas = generateBetas(transFactorsCount, regulatedGenesPerTF)
+# 
+# Xtu = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
+# Ytu = simulateResponse(Xtu, Betas)
+# 
+# Xtr = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
+# Ytr = simulateResponse(Xtr, Betas)
+# 
+# Xts = generateDataset(n, transFactorsCount, regulatedGenesPerTF)
+# Yts = simulateResponse(Xts, Betas)
+# 
+# lambdaGrid = 10 ^ seq(from = -2, by = 1, length = 6)
 
-startTime <- proc.time()
-graceSeq = grace(Xtr, Ytr[,1], Xtu, Ytu[,1], L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10)
-seqTime <- proc.time() - startTime
-seqTime <- seqTime[3]
+# startTime <- proc.time()
+# graceSeq = grace(Xtr, Ytr[,1], Xtu, Ytu[,1], L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10)
+# seqTime <- proc.time() - startTime
+# seqTime <- seqTime[3]
 
-cl = makeCluster(6, type = "PSOCK", port=10101)
-clusterSetRNGStream(cl, 0)
-startTime <- proc.time()
-gracePar = grace(Xtr, Ytr[,1], Xtu, Ytu[,1], L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10, parallel = TRUE, cl = cl)
-parTime <- proc.time() - startTime
-parTime <- parTime[3]
+# cl = makeCluster(6, type = "PSOCK", port=10101)
+# clusterSetRNGStream(cl, 0)
+# startTime <- proc.time()
+# gracePar = grace(Xtr, Ytr[,1], Xtu, Ytu[,1], L, lambdaGrid, lambdaGrid, lambdaGrid, K = 10, parallel = TRUE, cl = cl)
+# parTime <- proc.time() - startTime
+# parTime <- parTime[3]
 
 # stopCluster(cl)
 
-graceRes = gracePar
-prediction <- predict(object=graceRes$fit, as.matrix(Xts), type="response")
-errors = evalErrors(Yts[,1],prediction)
-betaHat = as.numeric(unlist(graceRes$coefficients$beta))
-trueBeta = as.numeric(Betas[1,])
-stat = evalBetaStatistics(trueBeta = as.numeric(Betas[1,]), betaHat = as.numeric(unlist(graceRes$coefficients$beta)))
-tuning = graceRes$parameters$errors
-
-batch = runBatch(n, transFactorsCount, regulatedGenesPerTF)
-
-glmmod <- glmnet(x, y=as.factor(asthma), alpha=1, family="binomial") # Lasso
+# graceRes = gracePar
+# prediction <- predict(object=graceRes$fit, as.matrix(Xts), type="response")
+# errors = evalErrors(Yts[,1],prediction)
+# betaHat = as.numeric(unlist(graceRes$coefficients$beta))
+# trueBeta = as.numeric(Betas[1,])
+# stat = evalBetaStatistics(trueBeta = as.numeric(Betas[1,]), betaHat = as.numeric(unlist(graceRes$coefficients$beta)))
+# tuning = graceRes$parameters$errors
 
 # save.image("LiLi2008Big.rda")
 # load("LiLi2008Big.rda")
