c8da4506d55675728f40f7c4b7866485d562b5fa
SivoDaskalov <sivodaskalov@gmail.com>
2017-02-11 20:00:15 +0000

    N100 P2200 execution stored

--- main.R
+++ main.R
@@ -2,8 +2,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("batch_tools.R")
 
 n = 100
-transFactorsCount = 5
+transFactorsCount = 200
 regulatedGenesPerTF = 10
 
 batchResults = runBatch(n, transFactorsCount, regulatedGenesPerTF)
 unpackBatchResults(batchResults)
+save.image("data/N100P2200.RData")
