3d5c79e8e6565dab7ffb8eb912005a8fd56337af
SivoDaskalov <sivodaskalov@gmail.com>
2017-02-16 00:20:59 +0000

    Matlab code cleanup

--- cvx_cv_target.m
+++ cvx_cv_target.m
@@ -1,19 +1,12 @@
-p = dlmread('temp_in_p.txt');
-y = dlmread('temp_in_y.txt');
-x = dlmread('temp_in_x.txt');
-folds = dlmread('temp_in_folds.txt');
-lam_all = dlmread('temp_in_lam_all.txt');
-gam_all = dlmread('temp_in_gam_all.txt');
-
-results = zeros(size(y),2+1); % 2 = number of tuned parameters
-
-cd C:\Users\sivak_000\Documents\GitHub\dissertation\cvx;
-cvx_setup;
-cd('C:\Users\sivak_000\Documents\GitHub\dissertation');
-
+p = dlmread('tmp/in_p.txt');
+y = dlmread('tmp/in_y.txt');
+x = dlmread('tmp/in_x.txt');
+folds = dlmread('tmp/in_folds.txt');
+lam_all = dlmread('tmp/in_param_lam.txt');
+gam_all = dlmread('tmp/in_param_gam.txt');
+results = zeros(length(lam_all)*length(gam_all),2+1); % 2 = number of tuned parameters
 tStart=tic;
 k = max(folds);
-
 counter = 1;
 for lam_it=1:size(lam_all);
     lam=lam_all(lam_it);
@@ -23,7 +16,7 @@ for lam_it=1:size(lam_all);
         for fold = 1:k;
             training = find(folds ~= fold);
             holdout = find(folds == fold);
-            cvx_begin quiet;
+            cvx_begin ;
             variables b(p);
             minimize(square_pos(norm(y - x*b, 2)) / 2 + lam*norm(b, 1)*gam);
             cvx_end;
@@ -35,8 +28,6 @@ for lam_it=1:size(lam_all);
     end;
 end;
 time=toc(tStart);
-
-dlmwrite('temp_out_results.txt', results, 'precision', '%10.10f');
-dlmwrite('temp_out_time.txt', full(time), 'precision', '%10.10f');
-
+dlmwrite('tmp/out_results.txt', results, 'precision', '%10.10f');
+dlmwrite('tmp/out_time.txt', full(time), 'precision', '%10.10f');
 % exit;
