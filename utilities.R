8597d8d256a38a0f261dc7ddd6f8f2ed39e8169d
Sivaka <sivodaskalov@gmail.com>
2017-04-13 01:34:16 +0300

    Found a bug in netwknorm and implemented in R
    netwknorm was building a matrix with a single row instead of summing the
    row-wise norm of the m by 2 matrix

---
+++ utilities.R
@@ -0,0 +1,20 @@
+
+b = c(6, 5, 4, 3, 2, 1)
+edges = Matrix(data = c(1, 2, 1, 3, 4, 5, 4, 6), nrow=4, ncol=2, byrow = TRUE)
+wt = c(1, 1, 1, 1, 1, 1)
+
+# x: a column vector of shape(p, 1)
+# wt: a column vector of shape(p, 1) 
+# netwk: a matrix of shape(m, 2)
+# gamma >= 1 is an integer constant or Inf 
+networkNorm = function(b, edges, wt, gamma) {
+  tmp = cbind(b[edges[, 1]] / wt[edges[, 1]], b[edges[, 2]] / wt[edges[, 2]])
+  if (is.infinite(gamma)) {
+    return(sum(apply(tmp, 1, function(x)
+      norm(as.matrix(x), type = "I"))))
+  }
+  return(sum(apply(tmp, 1, function(x)
+    sum(abs(x) ^ gamma) ^ (1 / gamma))))
+}
+
+networkNorm(b, edges, wt, Inf)
