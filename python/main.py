f6760f647a525a74e99a039bca032b2a2c003e47
SivoDaskalov <sivodaskalov@gmail.com>
2017-05-19 12:24:30 +0100

    Model similarity metrics added

--- python/main.py
+++ python/main.py
@@ -17,14 +17,14 @@ p = n_trans_factors * (n_regulated_genes_per_trans_factor + 1)
 relevant_trans_factor_groups = [1, 2, 3, 4, 5]
 
 setups = batch_generate_setups(n_regulated_genes_per_trans_factor=n_regulated_genes_per_trans_factor,
-                               n_trans_factors=n_trans_factors, load_dump=False,
+                               n_trans_factors=n_trans_factors, load_dump=True,
                                n_relevant_trans_factor_groups=relevant_trans_factor_groups,
                                n_tune_obs=200, n_train_obs=100, n_test_obs=100)
 
 
 def cv_mse_tune_generated_data(methods, load_dump=False):
     pd.set_option('display.width', 200)
-    fits = batch_fit_models(setups, methods=methods, load_dump=load_dump)
+    fits, similarities = batch_fit_models(setups, methods=methods, load_dump=load_dump)
     results = batch_evaluate_models(fits)
     print(results)
 
@@ -46,7 +46,7 @@ def fit_optimal_parameter_models_on_real_data(methods=real_data_methods, load_du
     export_errors(results)
 
 
-cv_mse_tune_generated_data(methods=["lasso", "enet"], load_dump=False)
+cv_mse_tune_generated_data(methods=["lasso", "enet"], load_dump=True)
 # load_custom_start_points("results/p550.csv")
 # orchestrated_tune_generated_data(opt_method="coef_correlation", load_dump=False)
 # orchestrated_tune_generated_data(opt_method="n_predictors", load_dump=False)
