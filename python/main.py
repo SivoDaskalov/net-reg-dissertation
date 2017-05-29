caca85dc83e57ba505394a2ec55f95b1f2ce9df1
SivoDaskalov <sivodaskalov@gmail.com>
2017-05-30 00:08:41 +0100

    Mappings updated with Linf models

--- python/main.py
+++ python/main.py
@@ -20,9 +20,9 @@ relevant_trans_factor_groups = [1, 2, 3, 4, 5]
 
 setups = None
 
-setups = batch_generate_setups(n_regulated_genes_per_trans_factor=n_regulated_genes_per_trans_factor, load_dump=True,
-                               n_trans_factors=n_trans_factors, train_on_tuning_dataset=True, n_tune_obs=300,
-                               n_test_obs=100, n_relevant_trans_factor_groups=relevant_trans_factor_groups)
+# setups = batch_generate_setups(n_regulated_genes_per_trans_factor=n_regulated_genes_per_trans_factor, load_dump=True,
+#                                n_trans_factors=n_trans_factors, train_on_tuning_dataset=True, n_tune_obs=300,
+#                                n_test_obs=100, n_relevant_trans_factor_groups=relevant_trans_factor_groups)
 
 
 def cv_mse_tune_generated_data(methods, load_dump=False):
@@ -63,14 +63,14 @@ def summarize_mapping_results():
 # cv_mse_tune_generated_data(
 #     methods=["lasso", "enet", "grace", "agrace", "gblasso", "linf", "alinf", "ttlp", "ltlp", "composite"],
 #     load_dump=True)
-load_custom_start_points("results/p550.csv")
-orchestrated_tune_generated_data(opt_method="coef_correlation", load_dump=True)
+# load_custom_start_points("results/p550.csv")
+# orchestrated_tune_generated_data(opt_method="coef_correlation", load_dump=True)
 
 # plot_summary_comparison(summary_urls={
 #     "CV of MSE tuning": "results/p550_summary.csv",
 #     "Orchestrated tuning": "results/p550_summary.csv"})
 # plot_parameter_tuning(results_file_urls=["results/p550.csv", "results/p550.csv"])
 
-# fit_optimal_parameter_models_on_real_data(methods=["lasso", "enet", "grace", "linf", "composite"], load_dump=True)
-# summarize_mapping_results()
+fit_optimal_parameter_models_on_real_data(methods=["lasso", "enet", "grace", "linf", "composite"], load_dump=True)
+summarize_mapping_results()
 print("Total time elapsed: %.0f seconds" % time.clock())
