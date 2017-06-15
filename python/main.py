18392acff74bf20581d5100983ec1581376bb431
SivoDaskalov <sivodaskalov@gmail.com>
2017-06-15 11:25:55 +0100

    Source code exported for submission

--- python/main.py
+++ python/main.py
@@ -18,13 +18,9 @@ n_trans_factors = 50
 n_regulated_genes_per_trans_factor = 10
 p = n_trans_factors * (n_regulated_genes_per_trans_factor + 1)
 relevant_trans_factor_groups = [1, 2, 3, 4, 5]
-
-setups = None
-
-
-# setups = batch_generate_setups(n_regulated_genes_per_trans_factor=n_regulated_genes_per_trans_factor, load_dump=True,
-#                                n_trans_factors=n_trans_factors, train_on_tuning_dataset=True, n_tune_obs=300,
-#                                n_test_obs=100, n_relevant_trans_factor_groups=relevant_trans_factor_groups)
+setups = batch_generate_setups(n_regulated_genes_per_trans_factor=n_regulated_genes_per_trans_factor, load_dump=True,
+                               n_trans_factors=n_trans_factors, train_on_tuning_dataset=True, n_tune_obs=300,
+                               n_test_obs=100, n_relevant_trans_factor_groups=relevant_trans_factor_groups)
 
 
 def cv_mse_tune_generated_data(methods, load_dump=False):
@@ -41,8 +37,8 @@ def cv_mse_tune_generated_data(methods, load_dump=False):
 def orchestrated_tune_generated_data(load_dump=False, opt_method="coef_correlation"):
     pd.set_option('display.width', 200)
     model_dump_url = "dumps/cache/orctun_models_%s_p%d" % (opt_method, p)
-    # orctun_fits = batch_do_orchestrated_tuning(setups, load_dump=load_dump, opt_method=opt_method)
-    # dump(orctun_fits, model_dump_url)
+    orctun_fits = batch_do_orchestrated_tuning(setups, load_dump=load_dump, opt_method=opt_method)
+    dump(orctun_fits, model_dump_url)
     orctun_fits = load(model_dump_url)
 
     recalculate_composite_models = False
@@ -76,27 +72,27 @@ def summarize_mapping_results():
     plot_mapping_fraction_votes()
 
 
-# cv_mse_tune_generated_data(
-#     methods=["lasso", "enet", "grace", "agrace", "gblasso", "linf", "alinf", "ttlp", "ltlp", "composite"],
-#     load_dump=True)
-# load_custom_start_points("results/p550.csv")
-# orchestrated_tune_generated_data(opt_method="coef_correlation", load_dump=True)
-# plot_convergence_statistics()
+cv_mse_tune_generated_data(
+    methods=["lasso", "enet", "grace", "agrace", "gblasso", "linf", "alinf", "ttlp", "ltlp", "composite"],
+    load_dump=True)
+load_custom_start_points("results/p550.csv")
+orchestrated_tune_generated_data(opt_method="coef_correlation")
+plot_convergence_statistics()
 
-# plot_summary_comparison(summary_urls={"CV of MSE tuning": "results/p550_summary.csv"},
-#                         figure_url="figures/cv_mse_tuning.png",
-#                         suptitle="Model metrics by regression method, CV-MSE tuning")
-# plot_summary_comparison(color="#FF7F0E",
-#     summary_urls={"Orchestrated tuning": "results/orctun_results_coef_correlation_p550_summary.csv"},
-#     figure_url="figures/orchestrated_tuning.png",
-#     suptitle="Model metrics by regression method, Orchestrated tuning")
-# plot_summary_comparison(summary_urls={
-#     "CV-MSE tuning": "results/p550_summary.csv",
-#     "Orchestrated tuning": "results/orctun_results_coef_correlation_p550_summary.csv"},
-#     figure_url="figures/tuning_method_comparison.png",
-#     suptitle="Model metrics comparison by tuning method")
-# plot_parameter_tuning(results_file_urls=["results/p550.csv", "results/orctun_results_coef_correlation_p550.csv"])
+plot_summary_comparison(summary_urls={"CV of MSE tuning": "results/p550_summary.csv"},
+                        figure_url="figures/cv_mse_tuning.png",
+                        suptitle="Model metrics by regression method, CV-MSE tuning")
+plot_summary_comparison(color="#FF7F0E",
+    summary_urls={"Orchestrated tuning": "results/orctun_results_coef_correlation_p550_summary.csv"},
+    figure_url="figures/orchestrated_tuning.png",
+    suptitle="Model metrics by regression method, Orchestrated tuning")
+plot_summary_comparison(summary_urls={
+    "CV-MSE tuning": "results/p550_summary.csv",
+    "Orchestrated tuning": "results/orctun_results_coef_correlation_p550_summary.csv"},
+    figure_url="figures/tuning_method_comparison.png",
+    suptitle="Model metrics comparison by tuning method")
+plot_parameter_tuning(results_file_urls=["results/p550.csv", "results/orctun_results_coef_correlation_p550.csv"])
 
-# fit_optimal_parameter_models_on_real_data(methods=["lasso", "enet", "grace", "linf", "composite"], load_dump=True)
-# summarize_mapping_results()
+fit_optimal_parameter_models_on_real_data(methods=["lasso", "enet", "grace", "linf", "composite"])
+summarize_mapping_results()
 print("Total time elapsed: %.0f seconds" % time.clock())
